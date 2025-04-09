9e9d58e4c45f141994d3b8a02c154375dfc1158f
Tristan Emma <temma22.57@gmail.com>
2025-04-09 15:51:54 -0700

    raw firsts

--- src/model/CardStack.java
+++ src/model/CardStack.java
@@ -1,5 +1,34 @@
 package model;
 
-public class CardStack {
+public class CardStack implements Iterable<Card> {
 
+	private final List<Card> cards = new ArrayList<Card>();
+	
+	public void push(Card c){
+		assert c != null && !cards.contains(c);
+		cards.add(c);
+	}
+	
+	public Card pop() {
+		assert !isEmpty();
+		return cards.remove(cards.size() - 1);
+	}
+	
+	public Card peek() {
+		assert !isEmpty();
+		return cards.get(cards.size() - 1);
+	}
+	
+	public void clear () {
+		cards.clear();
+	}
+	
+	public boolean isEmpty() {
+		return cards.size() == 0;
+	}
+	
+	public Iterator<Card> iterator() {
+		return cards.iterator();
+	}
+	
 }
---
+++ src/model/Suit.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Suit {
+	SPADES,DIAMONDS,CLUBS,HEARTS;
+}
